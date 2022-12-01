fb1236a3d3b1ef45566097a51cb21a024c9ac077
this-is-spear <rjsckdd12@gmail.com>
2022-12-01 17:22:59 +0900

    Init Project

---
+++ src/main/java/hello/tis/springatmapi/SpringAtmApiApplication.java
@@ -0,0 +1,13 @@
+package hello.tis.springatmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAtmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAtmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/tis/springatmapi/SpringAtmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.tis.springatmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAtmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
